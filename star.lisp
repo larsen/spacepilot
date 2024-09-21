ddada66e63cadc9813860f17b06cb8e46bcbfe67
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-09-21 21:37:45 +0200

    optimization (?)

--- star.lisp
+++ star.lisp
@@ -1,7 +1,7 @@
 (in-package #:spacepilot)
 
 (define-shader-entity star (vertex-entity transformed-entity colored-entity listener)
-  ((vertex-array :initform (// 'trial 'unit-sphere))
+  ((vertex-array :initform (// 'trial 'unit-cube))
    (color :initform (vec 0.8 0.8 0.8 1))
    (velocity :initform +player-speed+ :initarg :velocity :accessor velocity)))
 
