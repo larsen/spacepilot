4a3cbc80142a20f1edf1c2e23863e0ef422da7bb
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-09-27 21:51:14 +0200

    factored out world setup

--- world.lisp
+++ world.lisp
@@ -3,6 +3,16 @@
 (defclass world (pipelined-scene)
   ((enemy-spawn-timer :initform 0f0 :initarg :spawn-timer :accessor spawn-timer)))
 
+(defun setup-world (world)
+  (setf +spaceships+ (make-instance 'bag))
+  (enter +spaceships+ world)
+  (enter (make-instance 'player :name :player) +spaceships+)
+  (enter (init-starfield) world)
+  (preload (make-instance 'enemy) world)
+  (preload (make-instance 'explosion) world)
+  (preload (make-instance 'bullet :target :nobody) world)
+  (preload (// 'spacepilot-music 'background-music) world))
+
 (define-handler (world tick :before) ()
   (do-scene-graph (obj world)
     ;; We should use proper frustum culling, but at the moment these checks don't work
