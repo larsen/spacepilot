c951f0e63912aadcd987ded03f4b4ae63669ee2a
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2025-08-02 14:27:52 +0200

    moved shader entities into a separate directory

--- entities/exhaust.lisp
+++ entities/exhaust.lisp
@@ -5,9 +5,9 @@
   (:default-initargs :max-particles 100
                      :particle-rate 60
                      :texture (// 'spacepilot-images 'exhaust-emitter)
-                     :particle-force-fields `((:type :direction :strength 5.0)
-                                              (:type :vortex :strength 10.0))
-                     :particle-options `(:velocity 5.0 :randomness 0.5 :size 1.0 :scaling 2.0
+                     ;; :particle-force-fields `((:type :direction :strength 5.0)
+                     ;;                          (:type :vortex :strength 10.0))
+                     :particle-options `(:velocity 5.0 :randomness 0.5 :size 1.0 :scaling 1.0
                                          :color ,(vec3 0.75 0.75 0.90)
                                          :lifespan 0.25 :lifespan-randomness 0.5)))
 
