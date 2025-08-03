5949120c64c8b12c455342f73f12df3759aea870
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2025-08-03 19:30:20 +0200

    restored enemies spawn

--- world.lisp
+++ world.lisp
@@ -38,5 +38,4 @@
   (when (> (spawn-timer world) 3)
     (setf (spawn-timer world) 0)
     ;; This will make each individual enemy to enter the scene
-;    (make-instance 'squadron :scene +spaceships+)
-    ))
+   (make-instance 'squadron :scene +spaceships+)))
