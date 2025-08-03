17c8ac1f7259c4f5a37b34c882b818c9001231d4
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2025-08-03 11:50:25 +0200

    factored out the exhaust entity in a separate file

---
+++ exhaust.lisp
@@ -0,0 +1,18 @@
+(in-package #:spacepilot)
+
+(define-shader-entity engine-exhaust (cpu-particle-emitter)
+  ((spaceship-emitter :initarg :emitted-by :accessor spaceship-emitter))
+  (:default-initargs :max-particles 100
+                     :particle-rate 60
+                     :texture (// 'spacepilot-images 'exhaust-emitter)
+                     ;; :particle-force-fields `((:type :direction :strength 5.0)
+                     ;;                          (:type :vortex :strength 10.0))
+                     :particle-options `(:velocity 5.0 :randomness 0.5 :size 1.0 :scaling 1.0
+                                         :color ,(vec3 0.75 0.75 0.90)
+                                         :mode :quad
+                                         :lifespan 0.25 :lifespan-randomness 0.5)))
+
+(define-handler (engine-exhaust tick :after) ()
+  (debug-draw (vertex-array engine-exhaust))
+  (setf (orientation engine-exhaust) (orientation (spaceship-emitter engine-exhaust)))
+  (setf (location engine-exhaust) (location (spaceship-emitter engine-exhaust))))
