9f2d09a2a67072c0ef146067bff2a69f9af28ce4
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2026-08-05 22:01:57 +0200

    changed package reference

--- package.lisp
+++ package.lisp
@@ -7,7 +7,7 @@
    (#:trial-alloy #:org.shirakumo.fraf.trial.alloy)
    (#:simple #:org.shirakumo.alloy.renderers.simple)
    (#:presentations #:org.shirakumo.alloy.renderers.simple.presentations)
-   (#:colors #:org.shirakumo.alloy.colored.colors)
+   (#:colors #:org.shirakumo.alloy.chroma.colors)
    (#:harmony #:org.shirakumo.fraf.harmony.user)
    (#:trial-harmony #:org.shirakumo.fraf.trial.harmony)
    (#:sequences #:org.shirakumo.trivial-extensible-sequences))
